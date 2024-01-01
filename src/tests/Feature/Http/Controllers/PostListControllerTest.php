31793a7d997d259b412dcbd9a8b75d4dd89612bf
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-01-01 22:55:13 +0900

    feat: ブログ一覧のテストするために必要なファイル追加

--- src/tests/Feature/Http/Controllers/PostListControllerTest.php
+++ src/tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\controllers;
 
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,8 +14,22 @@ class PostListControllerTest extends TestCase
     {
         // $this->withoutExceptionHandling();
 
+        // $post1 = Post::factory()->create();
+        // $post2 = Post::factory()->create();
+
+        // $this->get('/')
+        //     ->assertOk()
+        //     ->assertSee($post1->title)
+        //     ->assertSee($post2->title);
+
+        $post1 = Post::factory()->create(['title' => 'ブログのタイトル１']);
+        $post2 = Post::factory()->create(['title' => 'ブログのタイトル２']);
+
         $this->get('/')
-            ->assertOk();
+            ->assertOk()
+            ->assert()
+            ->assertSee('ブログのタイトル１')
+            ->assertSee('ブログのタイトル２');
     }
 
 }
