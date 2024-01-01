4f48f4936634dfda4a0079a85edcb5f08e18abaf
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-01-02 08:59:32 +0900

    ユーザーが複数Postを所有するDatabaseSeederに変更

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Post;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,6 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Post::factory(30)->create();
+        // Post::factory(30)->create();
+
+        User::factory(15)->create()->each(function ($user) {
+            Post::factory(random_int(2, 5))->create(['user_id' => $user]);
+        });
     }
 }
