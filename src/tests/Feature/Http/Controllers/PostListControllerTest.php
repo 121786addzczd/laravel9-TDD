540bf370237eaf1b4a0afd4c3d13547f2fe5c2c5
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-01-01 23:46:38 +0900

    feat: postsテーブルのカラムに外部キーのusser_idを追加

--- src/tests/Feature/Http/Controllers/PostListControllerTest.php
+++ src/tests/Feature/Http/Controllers/PostListControllerTest.php
@@ -27,7 +27,6 @@ class PostListControllerTest extends TestCase
 
         $this->get('/')
             ->assertOk()
-            ->assert()
             ->assertSee('ブログのタイトル１')
             ->assertSee('ブログのタイトル２');
     }
