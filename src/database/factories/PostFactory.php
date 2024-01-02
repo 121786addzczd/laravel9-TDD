66efe400d2f8a216d04d45309e2737e611452103
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-01-02 10:26:23 +0900

    feat: ブログの公開フラグ追加

--- src/database/factories/PostFactory.php
+++ src/database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -19,8 +20,20 @@ class PostFactory extends Factory
     {
         return [
             'user_id' => User::factory(),
+            'status' => Post::OPEN,
             'title' => $this->faker->realText(20),
             'body' => $this->faker->realText(200),
         ];
+    }
+
+    // Postエンティティのstatus属性にランダムな値を設定するstate定義
+    // Post::OPENの確率を高めるために、配列に複数回含めている。
+    public function random()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'status' => $this->faker->randomElement([Post::OPEN, Post::OPEN, Post::OPEN ,Post::OPEN, Post::CLOSED]),
+            ];
+        });
     }
 }
