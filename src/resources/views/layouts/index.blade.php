bc8d6fa6b8e94a432b228150ed69d57f9e3fa2d8
121786addzczd <72346611+121786addzczd@users.noreply.github.com>
2024-01-02 20:01:32 +0900

    feat: ブラウザの表示追加と修正

--- src/resources/views/layouts/index.blade.php
+++ src/resources/views/layouts/index.blade.php
@@ -7,6 +7,21 @@
 </head>
 
 
+<nav>
+    <li><a href="/">TOP（ブログ一覧）</a></li>
+
+    @auth
+        <li><a href="{{ route('mypage.posts') }}">マイブログ一覧</a></li>
+        <li>
+            <form method="post" action="{{ route('logout') }}">
+            @csrf<input type="submit" value="ログアウト">
+            </form>
+        </li>
+    @else
+        <li><a href="{{ route(('login')) }}">ログイン</a></li>
+    @endauth
+</nav>
+
 @yield('content')
 
 
